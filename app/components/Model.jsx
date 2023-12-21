e5b4dc15ca56332905228fab6f34d1e10d5f396a
winnyDiese <winnydiese@gmail.com>
2023-12-21 08:15:07 +0100

    Render background of model floues

--- app/components/Model.jsx
+++ app/components/Model.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Model = () => {
+const Model = ({isVisible}) => {
+    if((isVisible)) return null
     return (
-        <div>
-            Model
+        <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center '>
+            <div className='w-[600px] flex flex-col'>
+                <button className='text-white text-xl '>X</button>
+                <div className='bg-white p-2 rounded'>
+                    Model
+                </div>
+            </div>
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,12 +1,32 @@
-import Image from 'next/image'
+"use client"
+
+import Model from './components/Model'
+import { Fragment, useState } from 'react'
 
 export default function Home() {
+  const [showModal,setShowModal] = useState(false)
   return (
     <div className='p-10 text-center'>
-      <h1 className='text-xl mb-5'>Create custom model Nextjs with Windwind</h1>
-      <button className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 p-y-2.5 text-center mr-5'>Text model</button>
-      <button className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 p-y-2.5 text-center mr-5'>Video model</button>
-      <button className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 p-y-2.5 text-center mr-5'>Form model</button>
+      <Fragment>
+        <h1 className='text-xl mb-5'>Create custom model Nextjs with Windwind</h1>
+        <button 
+          onClick={()=>setShowModal(true)}
+          className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5'
+        >
+          Text model
+        </button>
+        <button 
+          className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5'
+        >
+          Video model
+        </button>
+        <button 
+          className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5'
+        >
+           Form model
+        </button>
+        <Model isVisible={showModal} onClosse={()=>setShowModal(false)} />
+      </Fragment>
     </div>
   )
 }
