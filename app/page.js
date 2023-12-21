edd24eaba839e6f47a7b089e78a198b5a0a61122
winnyDiese <winnydiese@gmail.com>
2023-12-21 07:47:47 +0100

    Add tree button model

--- app/page.js
+++ app/page.js
@@ -3,8 +3,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className='p-10 text-center'>
-      <h1 className='text-xl'>Create custom model Nextjs with Windwind</h1>
+      <h1 className='text-xl mb-5'>Create custom model Nextjs with Windwind</h1>
       <button className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 p-y-2.5 text-center mr-5'>Text model</button>
+      <button className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 p-y-2.5 text-center mr-5'>Video model</button>
+      <button className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 p-y-2.5 text-center mr-5'>Form model</button>
     </div>
   )
 }
